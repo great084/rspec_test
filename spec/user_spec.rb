959ccc7860f1d537e9e108f51498da00ac60d193
great084 <great084@gmail.com>
2020-08-18 11:52:38 +0900

    user_specのリファクタ

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,19 +3,15 @@ require 'spec_helper'
 RSpec.describe User do
   describe "#greet" do
     # before do
-    let(:user){ User.new(params)}
-    let(:params) { { name: "たろう", age: age} }
+    let(:user){ User.new(name: "たろう", age: age) }
+    subject { user.greet }
     context '12歳以下の場合' do
       let(:age){ 12 }
-      it "ひらがなで答える" do
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
-      end
+      it {is_expected.to eq 'ぼくはたろうだよ。'}
     end
     context '13歳以上の場合' do
       let(:age){ 13 }
-      it "漢字で答える" do
-        expect(user.greet).to eq '僕はたろうです。'
-      end
+      it {is_expected.to eq '僕はたろうです。'}
     end
   end
 end
